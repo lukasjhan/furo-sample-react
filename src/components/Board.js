479d9ed124f09c6822ce9e1976febb45197967da
Ace Shim <ace@hopae.io>
2022-10-26 04:56:29 +0900

    Added user info

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,12 +1,12 @@
-import React, { useContext } from "react";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { coldarkDark } from "react-syntax-highlighter/dist/esm/styles/prism";
-import { Button, Typography, Tooltip } from "antd";
-import { useFuro } from "furo-react";
-import { WarningOutlined } from "@ant-design/icons";
-import styles from "../styles/board.module.css";
-import { ConfigContext } from "../contexts/ConfigContext";
-import { ImportOutlined } from "@ant-design/icons";
+import React, { useContext } from 'react';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { coldarkDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { Button, Typography, Tooltip } from 'antd';
+import { useFuro } from 'furo-react';
+import { WarningOutlined } from '@ant-design/icons';
+import styles from '../styles/board.module.css';
+import { ConfigContext } from '../contexts/ConfigContext';
+import { ImportOutlined } from '@ant-design/icons';
 
 const { Text } = Typography;
 
@@ -20,16 +20,16 @@ const Board = () => {
         {user ? (
           <>
             <SyntaxHighlighter
-              language="javascript"
+              language='javascript'
               style={coldarkDark}
               wrapLines={true}
               wrapLongLines={true}
             >
-              {JSON.stringify(user, null, "\t")}
+              {'// User Info\n' + JSON.stringify(user, null, '\t')}
             </SyntaxHighlighter>
 
             <div className={styles.button_wrapper}>
-              <Tooltip title={"로그아웃"}>
+              <Tooltip title={'로그아웃'}>
                 <ImportOutlined
                   className={styles.icon_logout}
                   onClick={logout}
@@ -40,29 +40,29 @@ const Board = () => {
         ) : (
           <>
             {!hasClientId && (
-              <Text type="danger" className={styles.warning}>
+              <Text type='danger' className={styles.warning}>
                 <WarningOutlined className={styles.icon_warning} />
                 <Tooltip
                   title={
                     <a
-                      href="https://github.com/furo-official/furo-sample-react#%ED%99%98%EA%B2%BD-%EC%84%A4%EC%A0%95"
-                      target={"_blank"}
-                      rel="noreferrer"
+                      href='https://github.com/furo-official/furo-sample-react#%ED%99%98%EA%B2%BD-%EC%84%A4%EC%A0%95'
+                      target={'_blank'}
+                      rel='noreferrer'
                     >
                       설정하러 가기
                     </a>
                   }
-                  color={"#ffffff10"}
+                  color={'#ffffff10'}
                 >
                   Client ID
-                </Tooltip>{" "}
+                </Tooltip>{' '}
                 가 설정되지 않았습니다
               </Text>
             )}
             <Button
-              type="primary"
-              size="large"
-              shape="round"
+              type='primary'
+              size='large'
+              shape='round'
               onClick={loginWithRedirect}
               disabled={!hasClientId}
             >
